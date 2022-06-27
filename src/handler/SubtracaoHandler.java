e6c355b049835355cf7b0d207188e7aced966992
claracortez1 <mcccr1@aluno.ifal.edu.br>
2022-06-27 15:20:18 +0000

    Add tratamento de erros

--- src/handler/SubtracaoHandler.java
+++ src/handler/SubtracaoHandler.java
@@ -1,6 +1,7 @@
 package src.handler;
 
 import static java.net.HttpURLConnection.HTTP_OK;
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -15,26 +16,30 @@ public class SubtracaoHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange conn) throws IOException {
+        try{
 
-        String [] partes = conn.getRequestURI().getPath().split("/");
-        byte[] result = subtrair (partes[2], partes[3]);
-        
-        try {
-            conn.sendResponseHeaders(HTTP_OK, result.length);
+            String [] parameters = conn.getRequestURI().getPath().split("/");
 
-            Headers headers = conn.getResponseHeaders();
-            headers.add("Content-Type", "text/html; charset=UTF-8");
-
-            try (OutputStream out = conn.getResponseBody()) {
-                out.write(result);
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
+            if(parameters.length > 4){
+                throw new IOException("Você não pode passar mais que 2 parâmetros");
+            } else if (parameters.length < 4) {
+                throw new IOException("Você não pode passar menos que 2 parâmetros");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+            byte[] response = subtrair (parameters[2], parameters[3]);
+            
+            try {
+                response(HTTP_OK, response, conn);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (NumberFormatException e){
+            response(HTTP_BAD_REQUEST, "Formato de parâmetro inválido".getBytes(), conn);
+        } catch(IOException e){
+            response(HTTP_BAD_REQUEST, e.getMessage().getBytes(), conn);
         } finally {
             conn.close();
-        }            
+        }
     }
 
     byte[] subtrair(String parametro1, String parametro2) {
@@ -44,6 +49,21 @@ public class SubtracaoHandler implements HttpHandler {
         double resultado = num1 - num2;
 
         return Double.toString(resultado).getBytes();
+    }
+
+
+    void response(int HTTP_CODE, byte[] response, HttpExchange conn) throws IOException{
+
+        conn.sendResponseHeaders(HTTP_CODE, response.length);
+
+        Headers headers = conn.getResponseHeaders();
+        headers.add("Content-Type", "text/html; charset=iso-9881");
+
+        try(OutputStream out = conn.getResponseBody()) {
+            out.write(response);
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
     }
 
 }
