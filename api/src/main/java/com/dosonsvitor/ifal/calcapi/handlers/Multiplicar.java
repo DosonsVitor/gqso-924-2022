7ab28262143a0b88c429f8cfb903ea3f3651f5e0
Dosons <dosons123@gmail.com>
2022-07-25 11:25:50 -0300

    implementar função de multiplicação no handler

--- api/src/main/java/com/dosonsvitor/ifal/calcapi/handlers/Multiplicar.java
+++ api/src/main/java/com/dosonsvitor/ifal/calcapi/handlers/Multiplicar.java
@@ -1,14 +1,15 @@
 package com.dosonsvitor.ifal.calcapi.handlers;
 
-import io.jooby.annotations.GET;
-import io.jooby.annotations.Path;
-import io.jooby.annotations.PathParam;
+
+import io.jooby.Context;
 import io.jooby.annotations.*;
 
 @Path("multi/{parametro1}/{parametro2}")
 public class Multiplicar {
     @GET
-    public String Multi(@PathParam("parametro1") String par1){
-        return par1;
+    public Double Multi(@PathParam String parametro1, @PathParam String parametro2){
+        Double num1 = Double.parseDouble(parametro1);
+        Double num2 = Double.parseDouble(parametro2);
+        return num1 * num2;
     }
 }
