b5aa8f9825acee13e4aae4dec42e311e33588bb6
Verônica Nunes <103063127+m-veronica-n@users.noreply.github.com>
2022-07-26 16:22:06 -0300

    Create SomarTest.java

---
+++ api/src/test/java/com/dosonsvitor/ifal/calcapi/SomarTest.java
@@ -0,0 +1,45 @@
+package com.dosonsvitor.ifal.calcapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import io.jooby.MockRouter;
+import io.jooby.StatusCode;
+import io.jooby.exception.BadRequestException;
+
+public class SomarTest {
+    @Test
+    public void sum() {
+        MockRouter router = new MockRouter(new App());
+        router.get("/sum/5/5", rsp -> {
+            assertEquals(10.0, rsp.value());
+            assertEquals(StatusCode.OK, rsp.getStatusCode());
+        });
+    }
+
+    @Test
+    public void sum_firstOperadorIsString() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/sum/a/5", rsp -> {});
+        });
+    }
+
+    @Test
+    public void sum_secondOperadorIsString() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/sum/2/a", rsp -> {});
+        });
+    }
+
+    @Test
+    public void sum_allOperadorsAreString() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/sum/a/a", rsp -> {});
+        });
+    }
+}
