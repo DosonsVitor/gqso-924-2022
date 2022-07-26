c99231fbff743e54732670fa0e9e9f9636205a97
mariaclaraleao <90777796+mariaclaraleao@users.noreply.github.com>
2022-07-26 16:25:59 -0300

    Update Dividir.java

--- api/src/main/java/com/dosonsvitor/ifal/calcapi/handlers/Dividir.java
+++ api/src/main/java/com/dosonsvitor/ifal/calcapi/handlers/Dividir.java
@@ -7,8 +7,20 @@ import io.jooby.exception.BadRequestException;
 public class Dividir {
     @GET
     public Double Div(@PathParam String parametro1, @PathParam String parametro2) {
-        Double num1 = Double.parseDouble(parametro1);
-        Double num2 = Double.parseDouble(parametro2);
+        Double num1, num2;
+        try {
+            num1 = Double.parseDouble(parametro1);
+        } catch (NumberFormatException e) {
+            throw new BadRequestException("Parâmetro inválido: " + parametro1);
+        } try {
+            num2 = Double.parseDouble(parametro2);
+            if(num2 == 0){
+                throw new BadRequestException("Você não pode dividir por 0");
+            }
+        } catch (NumberFormatException e) {
+            throw new BadRequestException("Parâmetro inválido: " + parametro2);
+        }
+
         return num1 / num2;
     }
 }
