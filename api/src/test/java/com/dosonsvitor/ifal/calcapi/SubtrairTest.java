60be49a9ab2488a8d49d117ddf242fd87b9a4e1d
claracortez1 <103062915+claracortez1@users.noreply.github.com>
2022-07-26 14:03:00 -0300

    Create SubtrairTest.java

---
+++ api/src/test/java/com/dosonsvitor/ifal/calcapi/SubtrairTest.java
@@ -0,0 +1,45 @@
+package com.dosonsvitor.ifal.calcapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import io.jooby.MockRouter;
+import io.jooby.StatusCode;
+import io.jooby.exception.BadRequestException;
+
+public class SubtrairTest {
+    @Test
+    public void sub() {
+        MockRouter router = new MockRouter(new App());
+        router.get("/sub/5/5", rsp -> {
+            assertEquals(0.0, rsp.value());
+            assertEquals(StatusCode.OK, rsp.getStatusCode());
+        });
+    }
+
+    @Test
+    public void sub_firstOperadorIsString() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/sub/a/5", rsp -> {});
+        });
+    }
+
+    @Test
+    public void sub_secondOperadorIsString() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/sub/2/a", rsp -> {});
+        });
+    }
+
+    @Test
+    public void sub_allOperadorsAreString() {
+        MockRouter router = new MockRouter(new App());
+        assertThrows(BadRequestException.class, () ->{
+            router.get("/sub/a/a", rsp -> {});
+        });
+    }
+}
